7dffdbb23e416d17807cbc11f3ae16f4a57157ce
Alexander Fok <foktmalexander@gmail.com>
2021-03-28 05:19:41 -0700

    Add moving block in hook

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,17 +1,40 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import Board from '../Board';
 import './index.css'
 
 const Game = () => {
   // initialize a width * height board
-  const [width, height] = [7, 10];
-  const [board, setBoard] = useState(new Array(height).fill(new Array(width).fill(0)));
-  const [buildingBlock, setBuildingBlock] = useState({ position: 2, width: 3 })
-  // const [isGameOver, endGame] = useState(false);
-  // const timeout = 500;
-  let blockPosition = height - 1;
+  const [boardWidth, boardHeight] = [7, 10];
+  const [board, setBoard] = useState(new Array(boardHeight).fill(new Array(boardWidth).fill(0)));
+  const block = useRef({ position: 2, length: 3, direction: 1 }).current;
+  const timeout = 500;
+  let activeRow = useRef(boardHeight - 1).current;
 
-  return <Board board={board} />
+  useEffect(() => {
+    const {position, length, direction} = block;
+    const last = position + length;
+
+    // Get new Board
+    const newBoard = board.slice();
+    newBoard[activeRow] = new Array(board[0].length).fill(0).fill(1, position, last);
+
+    // Change block moving direction when in contact with boundary
+    const chgDir = (direction > 0 && last === boardWidth)  || (direction < 0 && position === 0);
+
+    // Update block variable
+    Object.assign(block, {
+      position: position + (chgDir ? direction * -1 : direction),
+      direction: chgDir ? direction * -1 : direction  
+    });
+
+    setTimeout( () => setBoard(newBoard), timeout)
+  });
+
+  return (
+      <Board
+        board={board}   
+      />
+  )
 }
 
 export default Game;
