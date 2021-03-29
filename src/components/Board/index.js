3b10474b00a258ad2d9c96df8fb5f46a6efad802
Alexander Fok <foktmalexander@gmail.com>
2021-03-28 23:27:56 -0700

    Change styling to allow overlay

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -5,21 +5,30 @@ const Board = (props) => {
   const {board} = props;
 
   return (
-    <div className="board">
+    <div className='board'>
       {board.map((row, rowIndex) => 
         <div
-          className="board-row"
+          className={`board-row ${props.isGameover ? 'blur' : ''}`}
           key={`row-${rowIndex}`}
         >
           { row.map((stat, colIndex) => {
-              const index = rowIndex * (row.length) + colIndex;
-              return stat === 0 
-                ? <Square key={`col-${index}`}/>
-                : <Square key={`col-${index}`} occupied={true} />
+            const index = rowIndex * (row.length) + colIndex;
+            return stat === 0 
+              ? <Square key={`col-${index}`}/>
+              : <Square key={`col-${index}`} occupied={true} />
           })}
         </div>
       )}
+      {
+        props.isGameover
+        ? <div className="overlay">
+            <p>GAME OVER</p>
+            <p>Your score is : {props.score}</p>
+          </div>
+        : ""
+      }
     </div>
+    
   );
 }
 
